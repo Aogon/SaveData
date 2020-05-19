5b75b71d65af0c884daa82622e43da6de73de17a
Aogon <aoi_sakai@icloud.com>
2020-05-19 21:18:42 +0900

    add new file

---
+++ app/src/main/java/app/sakai/tororoimo/savedata/MainActivity.kt
@@ -0,0 +1,14 @@
+package app.sakai.tororoimo.savedata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
